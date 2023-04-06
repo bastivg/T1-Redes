83abb083b9b80b18b9315fd39081e1138b2acf0e
bastivg <97237431+bastivg@users.noreply.github.com>
2023-04-05 22:08:15 -0400

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -25,7 +25,7 @@ s.sendto(msj, (host, port))
 # aunque solo usaremos el primer dato que se obtiene que contiene el mensaje que llega de vuelta, recordar decodificarlo.
 # el valor dentro de recvfrom es el buffer que va a leer de lo recibido.
 respuesta = s.recvfrom(1024)[0].decode()
-
+print(respuesta)
 elementos = respuesta[0].split()
 
 diccionario = {}
