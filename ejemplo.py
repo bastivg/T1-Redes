8689e66eef3ae4cbf341468ff1b0e204f5a0cdaf
bastivg <97237431+bastivg@users.noreply.github.com>
2023-04-06 01:22:50 -0400

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -3,10 +3,7 @@
 import socket
 
 
-print("hola soy los cambios")
 # definicion de host y puerto: Indicarán hacia donde nos estaremos conectando inicialmente
-
-#host = 'localhost'
 host = 'jdiaz.inf.santiago.usm.cl'
 port = 50006
 
@@ -35,7 +32,13 @@ for elemento in elementos:
     
 buffer = diccionario["W"] * diccionario["H"] * 3
 
-print(buffer)
+contador = 2
+if "P3UDP" or "P3TCP" in diccionario:
+    contador = 3
+   
+div_buffer = buffer/contador
+
+
 
 # Ejemplo para sacar y leer bytes de una foto
 '''
@@ -46,4 +49,26 @@ open("nombre.txt", "w") -> open("nombre.txt", "wb")
 '''
 
 ## NOTA: Mucho ojo con lo que hace encode y decode, recuerden que la codificación con .encode() transforma un texto a bytes. [para cuando escriban la foto por ejemplo]
+print(respuesta)
+
+
+#Llamada por TCP
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Paso 2 - Definir el mensaje a enviar: Debemos establecer un mensaje a enviar al servidor, recuerde codificarlo
+msj = "GET 1/" + str(contador) + " IMG ID:" + str(diccionario["ID"])
+msj = msj.encode()
+
+# Paso 3 - Enviar el mensaje: Teniendo el mensaje y el socket basta con enviar el mensaje deseado
+# se adjunta a la funcion el mensaje y una tupla con el host y puerto a comunicar
+s.sendto(msj, (host, port))
+
+# Paso 4 - Obtener la respuesta: Enviado el mensaje quead recibir la respuesta desde el servidor, siendo una lista con información,
+# aunque solo usaremos el primer dato que se obtiene que contiene el mensaje que llega de vuelta, recordar decodificarlo.
+# el valor dentro de recvfrom es el buffer que va a leer de lo recibido.
+respuesta = s.recvfrom(div_buffer)[0].decode()
+
+
 print(respuesta)
