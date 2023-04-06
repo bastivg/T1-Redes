c29b57f2c70e848bfdc772035de9977361a9a72b
bastivg <97237431+bastivg@users.noreply.github.com>
2023-04-05 20:18:58 -0400

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,36 @@
+# socket: La libreria a utilizar
+
+import socket
+
+# definicion de host y puerto: Indicarán hacia donde nos estaremos conectando inicialmente
+
+host = 'localhost'
+port = 50006
+
+# Un ejemplo en UDP
+# Paso 1 - Definir el socket: el segundo argumento permite definir el tipo de conexión
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Paso 2 - Definir el mensaje a enviar: Debemos establecer un mensaje a enviar al servidor, recuerde codificarlo
+msj = "Hola!".encode()
+
+# Paso 3 - Enviar el mensaje: Teniendo el mensaje y el socket basta con enviar el mensaje deseado
+# se adjunta a la funcion el mensaje y una tupla con el host y puerto a comunicar
+s.sendto(msj, (host, port))
+
+# Paso 4 - Obtener la respuesta: Enviado el mensaje quead recibir la respuesta desde el servidor, siendo una lista con información,
+# aunque solo usaremos el primer dato que se obtiene que contiene el mensaje que llega de vuelta, recordar decodificarlo.
+# el valor dentro de recvfrom es el buffer que va a leer de lo recibido.
+respuesta = s.recvfrom(1024)[0].decode()
+
+# Ejemplo para sacar y leer bytes de una foto
+'''
+Es el clásico de ejemplo de abrir y cerrar un archivo, solo que en vez de leer o escribir realizarlo con el comando read bytes y write bytes
+
+open("nombre.txt", "w") -> open("nombre.txt", "wb") 
+
+'''
+
+## NOTA: Mucho ojo con lo que hace encode y decode, recuerden que la codificación con .encode() transforma un texto a bytes. [para cuando escriban la foto por ejemplo]
+
+print(respuesta)
