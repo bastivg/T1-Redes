2a6ab579226b9dc5a5f2e4151a07a9dfe6b1fa92
bastivg <97237431+bastivg@users.noreply.github.com>
2023-04-07 21:57:16 -0400

    Update b.py

--- b.py
+++ b.py
@@ -12,18 +12,22 @@ port = 50006
 # Un ejemplo en UDP
 def llamado_1_udp(host_udp,port_udp):
     s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("Mensaje enviado a : <", port,"> por <UDP>: GET NEW IMG DATA")
     msj = "GET NEW IMG DATA".encode()
     s_udp.sendto(msj, (host_udp, port_udp))
     respuesta = s_udp.recvfrom(1024)[0].decode()
+    print("Mensaje recibido de: <", port,"> por <UDP>: <",respuesta,">")
     return respuesta
 
 def llamado_tcp(host_tcp,port_tcp,n): #n = n / 3
-    #port = diccionario["P1TCP"]
     s_tcp_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_tcp_1.connect((host_tcp,port_tcp))
+    msj_pt1 = ("GET " + str(n) + "/" + str(contador) + " IMG ID:" + str(diccionario["ID"]))
+    print("Mensaje enviado a ", diccionario["IP"], ": <", port_tcp,"> por <TCP>: <", msj_pt1, ">")
     msj_pt1 = ("GET " + str(n) + "/" + str(contador) + " IMG ID:" + str(diccionario["ID"])).encode('utf-8')
     s_tcp_1.sendto(msj_pt1, (host_tcp, port_tcp))
     respuesta_tcp = s_tcp_1.recvfrom(div_buffer)[0]
+    print("Mensaje recibido de: <", diccionario["IP"], ">:<", port_tcp,"> por <TCP>: <", respuesta_tcp, ">")
     return respuesta_tcp
 
 def llamado_2_udp(host_udp,port_udp,n):
@@ -41,7 +45,7 @@ def verificador(vef_b):
     s_tcp_pv.sendto(msj_pv, (host, diccionario["PV"]))
     respuesta_pv = s_tcp_pv.recvfrom(div_buffer)[0]
     print(respuesta_pv)
-    if respuesta_pv == "200: SUCCESS":
+    if respuesta_pv == "'200: SUCCESS'":
         return 0
     return 1
 
