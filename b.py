f3e73edd6bafa6fdc19ab453571acef187d156c6
bastivg <97237431+bastivg@users.noreply.github.com>
2023-04-08 00:30:59 -0400

    Update b.py

--- b.py
+++ b.py
@@ -4,10 +4,10 @@ import socket
 #from signal import signal, SIGPIPE, SIG_DFL
 #signal(SIGPIPE,SIG_DFL)
 
-
 # definicion de host y puerto: Indicarán hacia donde nos estaremos conectando inicialmente
 host = 'jdiaz.inf.santiago.usm.cl'
 port = 50006
+
 
 # Un ejemplo en UDP
 def llamado_1_udp(host_udp,port_udp):
@@ -42,70 +42,76 @@ def llamado_2_udp(host_udp,port_udp,n):
     return respuesta_pt2
 
 def verificador(vef_b):
+    print("verificando respuesta... este proceso puede tardar")
     s_tcp_pv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_tcp_pv.connect((host,diccionario["PV"]))
     msj_pv = vef_b
     s_tcp_pv.sendto(msj_pv, (host, diccionario["PV"]))
     respuesta_pv = s_tcp_pv.recvfrom(div_buffer)[0].decode()
-    print(respuesta_pv)
+    print("Se obtuvo respuesta!!!: ",respuesta_pv)
     if "200" in respuesta_pv:
-        print("entro al success")
         return 0
+    print("El proceso fallo uwu")
     return 1
 
+def creacion_img(rp1,rp2,rp3,n):
+    if n == 2:
+        i = verificador(rp1+rp2)
+        if i == 0:
+            f = open("itento.png", "wb")
+            f.write(rp1)
+            f.write(rp2)
+            f.close()
+            return False
+        else:
+            return True
+            
+    else:
+        i = verificador(rp1+rp2+rp3)
+        if i == 0:
+            f = open("itento.png", "wb")
+            f.write(rp1)
+            f.write(rp2)
+            f.write(rp3)
+            f.close()
+            return False
+        else:
+            return True
 
 
 
+veff = True
 
-respuesta1 = llamado_1_udp(host,port)
-elementos = respuesta1.split()
-diccionario = {}
-for elemento in elementos:
-    clave, valor = elemento.split(":")
-    diccionario[clave] = int(valor)
-    
-buffer = diccionario["W"] * diccionario["H"] * 3
+while veff:
+    print("Iniciando un nuevo Proceso \n>:D\n")
+    respuesta1 = llamado_1_udp(host,port)
+    elementos = respuesta1.split()
+    diccionario = {}
+    for elemento in elementos:
+        clave, valor = elemento.split(":")
+        diccionario[clave] = int(valor)
+        
+    buffer = diccionario["W"] * diccionario["H"] * 3
 
-contador = 2
-if "P3UDP" in diccionario:
-    contador = 3
-if "P3TCP" in diccionario:
-    contador = 3
-   
-div_buffer = int(buffer/contador)
+    contador = 2
+    if "P3UDP" in diccionario:
+        contador = 3
+    if "P3TCP" in diccionario:
+        contador = 3
+    div_buffer = int(buffer/contador)
 
+    #Llamada por TCP
+    respuesta_pt1 = llamado_tcp(host,diccionario["P1TCP"],1)
 
+    #Llamada por UDP
+    respuesta_pt2 = llamado_2_udp(host,diccionario["P2UDP"],2)
 
-
-
-#Llamada por TCP
-
-respuesta_pt1 = llamado_tcp(host,diccionario["P1TCP"],1)
-
-#Llamada por UDP
-
-respuesta_pt2 = llamado_2_udp(host,diccionario["P2UDP"],2)
-
-if "P3UDP" in diccionario:
-    respuesta_pt3 = llamado_2_udp(host,diccionario["P3UDP"],3)
-    i = verificador(respuesta_pt1+respuesta_pt2+respuesta_pt3)
-    print("probando verificador:", i)
-    f = open("itento.png", "wb")
-    f.write(respuesta_pt1)
-    f.write(respuesta_pt2)
-    f.write(respuesta_pt3)
-    f.close()
-elif "P3TCP" in diccionario:
-    respuesta_pt3 = llamado_tcp(host,diccionario["P3TCP"],3)
-    f = open("itento.png", "wb")
-    f.write(respuesta_pt1)
-    f.write(respuesta_pt2)
-    f.write(respuesta_pt3)
-    f.close()
-else :
-    i = verificador(respuesta_pt1+respuesta_pt2)
-    print("probando verificador: 0 = success ", i)
-    f = open("itento.png", "wb")
-    f.write(respuesta_pt1)
-    f.write(respuesta_pt2)
-    f.close()
+    #Llamada 3:
+    if "P3UDP" in diccionario:
+        respuesta_pt3 = llamado_2_udp(host,diccionario["P3UDP"],3)
+        veff = creacion_img(respuesta_pt1,respuesta_pt2,respuesta_pt3,3)
+    elif "P3TCP" in diccionario:
+        respuesta_pt3 = llamado_tcp(host,diccionario["P3TCP"],3)
+        veff = creacion_img(respuesta_pt1,respuesta_pt2,respuesta_pt3,3)
+    else:
+        veff = creacion_img(respuesta_pt1,respuesta_pt2,"",2)
